d40f401de71ebe114859cf13c4f07fc4aa61be9f
synod2 <synod2@naver.com>
2019-11-24 20:56:33 +0000

    second

--- parse.py
+++ parse.py
@@ -1,49 +1,79 @@
+#!/usr/bin/env python3
+#by synod2 
 import dpkt, dpkt.dns
+import json 
 import datetime
 import socket
 import os 
+import sys 
 
-def mac_addr(address):
-	return ':'.join('%02x' % ord(b) for b in address)
-	
+dirname = "packets/"
 
-for app in range(1,10):
-	for k in range((app-1)*3+1,app*3+1):
-		path = "packs/"+str(k)+".pcap"
+class App_db:
+	def __init__(self,appname):
+		self.db_filename = dirname+appname+"/"+appname+".json"
+		with open(self.db_filename) as json_file:
+			self.json_data = json.load(json_file)
+			self.iplist = self.json_data["iplist"]
+			
+	def save(self):
+		with open(self.db_filename,'w',encoding="utf-8") as save_file:
+			json.dump(self.json_data,save_file,indent="\t")
 		
-		with open(path, 'rb') as f:
-			pcap = dpkt.pcap.Reader(f)
-			addr = []	#make addr list. 
-			var = 100
-
-			for timestamp, buf in pcap:
-				eth = dpkt.ethernet.Ethernet(buf)
-				ip = eth.data
-				var = var - 1
-				
-				if var == 0:
-					break
-				
-				if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-					continue
-				
-				if ip.p != dpkt.ip.IP_PROTO_TCP:
-					continue
-				
-				try :
-					udp = ip.data
-				except : 
-					continue 
-				
-				tmp = socket.inet_ntoa(ip.src)
-				# if src ip is 10.8.0.1 -> only need dst ip. 
-				if (tmp != "10.8.0.1"):
-					addr.append(tmp)
-				else : #else -> src ip is 10.8.0.1
-					addr.append(socket.inet_ntoa(ip.dst))
+for (path,dir,files) in os.walk(dirname) :
+	for filename in files :
+		ext = os.path.splitext(filename)[-1]
+		if ext == '.pcap':
+			appname = path.replace(dirname,"")
+			open_name = path+"/"+filename
+			
+			try : 
+				with open(open_name,'rb') as f:
+					pcap = dpkt.pcap.Reader(f)
+					addr = []	#make addr list. 
+					var = 100
 		
-		ex_addr = list(set(addr))
-		#print ex_addr
-		print "-----in file "+str(k)+"------ size : "+str(os.path.getsize(path))
-		for i in ex_addr : 
-			print i+ " is : " +str(addr.count(i))
+					for timestamp, buf in pcap:
+						eth = dpkt.ethernet.Ethernet(buf)
+						ip = eth.data
+						var = var - 1
+						
+						if var == 0:
+							break
+						
+						if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+							continue
+						
+						if ip.p != dpkt.ip.IP_PROTO_TCP:
+							continue
+						
+						try :
+							udp = ip.data
+						except : 
+							continue 
+						
+						tmp = socket.inet_ntoa(ip.src)
+						# if src ip is 10.8.0.1 -> only need dst ip. 
+						if (tmp != "10.8.0.1"):
+							addr.append(tmp)
+						else : #else -> src ip is 10.8.0.1
+							addr.append(socket.inet_ntoa(ip.dst))
+				#ex_addr -> lists of Not duplicated address.
+				ex_addr = list(set(addr))
+				
+				#print ex_addr
+				print (appname+" -----in file  "+filename+"------ size : "+str(os.path.getsize(path)))
+				for i in ex_addr : 
+					print (i+ " is : " +str(addr.count(i)))
+				
+			
+			except : 
+				print ("file open error "+open_name)
+				
+				
+# test = App_db("example")
+# test.iplist.append("6.6.6.6")
+# print(test.iplist)
+# print(test.db_filename)
+
+# test.save()
